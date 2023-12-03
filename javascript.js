64f93af21114360d3ca10e5f27b22b83666cc977
Cade Larrabee <larrabeecade@gmail.com>
2023-12-03 18:19:14 -0500

    adding main function for computing inputs

--- javascript.js
+++ javascript.js
@@ -1,5 +1,51 @@
 //const btnrock = document.querySelector(".rock");
 
+//use switch case with operator
+// use objects for storage of numbers and object
+//
+const operators = {
+  divide: "/",
+  power: "^",
+  add: "+",
+  subtract: "-",
+  multiple: "*",
+};
+
+const currentCalc = {
+  inputOne,
+  inputTwo,
+  operator,
+};
+
+const operate = function (currentCalc) {
+  if (
+    currentCalc.inputOne === undefined ||
+    currentCalc.inputTwo === undefined ||
+    currentCalc.operator === undefined
+  ) {
+    console.log("Error, invalid input");
+    return "Error, invalid input";
+  }
+
+  switch (currentCalc.operator) {
+    case operators.add:
+      add(currentCalc.inputOne, currentCalc.inputTwo);
+      break;
+    case operators.subtract:
+      subtract(currentCalc.inputOne, currentCalc.inputTwo);
+      break;
+    case operators.multiply:
+      multiply(currentCalc.inputOne, currentCalc.inputTwo);
+      break;
+    case operators.power:
+      toPower(currentCalc.inputOne, currentCalc.inputTwo);
+      break;
+    case operators.divide:
+      divide(currentCalc.inputOne, currentCalc.inputTwo);
+      break;
+  }
+};
+
 const add = function (numInputOne, numInputTwo) {
   return numInputOne + numInputTwo;
 };
