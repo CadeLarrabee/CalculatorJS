fd0aa2bc9a3f3eb37cb0f5f70940dabd94de8ab5
Cade Larrabee <larrabeecade@gmail.com>
2023-12-03 17:27:11 -0500

    Adding basic calculator functions (add, subt, ect)

---
+++ javascript.js
@@ -0,0 +1,44 @@
+//const btnrock = document.querySelector(".rock");
+
+const add = function (numInputOne, numInputTwo) {
+  return numInputOne + numInputTwo;
+};
+
+const subtract = function (numInputOne, numInputTwo) {
+  return numInputOne - numInputTwo;
+};
+
+const sum = function (arrayInput) {
+  let sum = 0;
+  arrayInput.forEach((item) => (sum += parseInt(item)));
+  return sum;
+};
+
+const multiply = function (arrayInput) {
+  let sum = arrayInput[0];
+  arrayInput.forEach(function (item, index) {
+    if (index != 0) {
+      sum *= parseInt(item);
+    }
+  });
+  return sum;
+};
+
+const power = function (intInput, toPower) {
+  let sum = intInput;
+  for (let i = 0; i < toPower - 1; i++) {
+    sum *= intInput;
+  }
+  return sum;
+};
+
+const factorial = function (intInput) {
+  let sum = intInput;
+  if (intInput == 0) {
+    return 1;
+  }
+  for (let i = intInput - 1; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
+};
